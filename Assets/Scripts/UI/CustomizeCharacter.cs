a3b52c3883def9675869e474eaffcd699ed11ecd
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-02-22 14:18:14 +0100

    Prefabified character customization and reduced copy paste in its script. Also reordered colors.

--- Assets/Scripts/UI/CustomizeCharacter.cs
+++ Assets/Scripts/UI/CustomizeCharacter.cs
@@ -5,28 +5,34 @@ using UnityEngine;
 public class CustomizeCharacter : MonoBehaviour
 {
     private Player player;
+
+    private void SetColorForPlayer(int playerIndex, int color)
+    {
+        PlayerPrefs.SetInt(playerIndex == 0 ? "PlayerOneColor" : "PlayerTwoColor", color);
+        UpdatePlayer();
+    }
     public void SetColorForPlayerOne(int color)
     {
-        PlayerPrefs.SetInt("PlayerOneColor", color);
-        UpdatePlayer();
+        SetColorForPlayer(0, color);
     }
 
     public void SetColorForPlayerTwo(int color)
     {
-        PlayerPrefs.SetInt("PlayerTwoColor", color);
+        SetColorForPlayer(1, color);
+    }
+    private void SetHairForPlayer(int playerIndex, int hair)
+    {
+        PlayerPrefs.SetInt(playerIndex == 0 ? "PlayerOneHair" : "PlayerTwoHair", hair);
         UpdatePlayer();
     }
-    
     public void SetHairForPlayerOne(int hair)
     {
-        PlayerPrefs.SetInt("PlayerOneHair", hair);
-        UpdatePlayer();
+        SetHairForPlayer(0, hair);
     }
 
     public void SetHairForPlayerTwo(int hair)
     {
-        PlayerPrefs.SetInt("PlayerTwoHair", hair);
-        UpdatePlayer();
+        SetHairForPlayer(1, hair);
     }
     private void UpdatePlayer()
     {
